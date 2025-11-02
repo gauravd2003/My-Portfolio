123ef2b1b536ee794a24dfd8f642af5561d6b183
Gaurav Deshmukh <167860993+gauravd2003@users.noreply.github.com>
2025-11-02 21:21:25 +0530

    Revise portfolio layout and content
    Updated portfolio to reflect new skills, projects, and certificates.

--- portfolio.py
+++ portfolio.py
@@ -6,25 +6,26 @@ st.set_page_config(page_title="Gaurav Deshmukh | Portfolio", layout="wide")
 
 # --- HEADER ---
 st.title("Gaurav Deshmukh")
-st.subheader("Data Analyst | Machine Learning Engineer | Python Developer")
+st.subheader("Data Science | Data Analyst | Machine Learning Engineer")
 st.write("Pune, India | gauravdeshmukh9970@gmail.com")
 
 # --- ABOUT ---
 st.header("About Me")
 st.write("""
-I'm a B.Tech Artificial Intelligence student passionate about **data analysis**, **machine learning**, 
+I'm a B.Tech Artificial Intelligence student passionate about **data science**, **machine learning**, **GEN Ai**, 
 and **social impact analytics**.  
 I create dashboards and models that convert raw data into meaningful insights.
 """)
 
 # --- SKILLS ---
 st.header("Technical Skills")
-skills = {
-    "Python (Pandas, NumPy, Scikit-learn)": 95,
-    "Data Visualization (Power BI, Tableau, Plotly)": 92,
-    "Machine Learning (Random Forest, XGBoost)": 85,
-    "Natural Language Processing (NLTK, VADER)": 80,
-    "SQL & Databases": 75
+skills ={
+    "- Python : Pandas, NumPy, Scikit-learn":95,
+    "- Data Visualization :Power BI, Tableau, Plotly,":90,
+    "- Machine Learning :Random Forest, XGBoost":90,
+    "- Natural Language Processing : NLTK, VADER":85,
+    "- SQL & Databases":90,
+    "- EXCEL":90,
 }
 for skill, val in skills.items():
     st.write(f"{skill}")
@@ -39,6 +40,7 @@ st.write("""
 - **Tools:** Power BI, Python (Pandas, Plotly)
 - Analyzed 50K+ CSR records to track beneficiaries and fund utilization across 6 states.
 - Improved decision-making efficiency by **30%**.
+- [PROJECT-1](https://github.com/gauravdeshmukh9970)
 """)
 
 # Women Empowerment NLP
@@ -47,6 +49,7 @@ st.write("""
 - **Tools:** Python (NLTK, VADER), Tableau  
 - Processed 15K+ posts and survey responses to evaluate public sentiment.  
 - Achieved **82% accuracy** in sentiment classification.
+- [PROJECT-2](https://github.com/gauravdeshmukh9970)
 """)
 
 # NGO Fund Prediction
@@ -55,20 +58,35 @@ st.write("""
 - **Tools:** Python (Scikit-learn, Power BI)  
 - Built predictive model estimating project impact score.  
 - Improved allocation efficiency by **20%**.
+- [PROJECT-3](https://github.com/gauravdeshmukh9970)
 """)
 
 # --- RESUME & CERTIFICATES ---
 st.header("Resume & Certificates")
 st.download_button(
     label="Download My Resume",
-    data=open("assets/RESUME.docx", "rb").read(),
-    file_name="RESUME.docx"
+    data=open("assets/RESUME.pdf", "rb").read(),
+    file_name="RESUME.pdf"
 )
 st.write("- Data Science with GenAI – NareshIT")
 st.write("- Machine Learning with Python – FreeCodeCamp")
+st.download_button(
+        label="CERTIFICATE - FreeCodeCamp",
+        data=open("assets/freecodecamp.pdf", "rb").read(),
+        file_name="freecodecamp.pdf"
+)
 st.write("- The Joy of Computing using Python – NPTEL")
+st.download_button(
+        label="CERTIFICATE - NPTEL",
+        data=open("assets/NPTEL.pdf", "rb").read(),
+        file_name="NPTEL.pdf"
+)
 st.write("- Data Analytics – Deloitte")
-
+st.download_button(
+        label="CERTIFICATE - Deloitte",
+        data=open("assets/Deloitte.pdf", "rb").read(),
+        file_name="Deloitte.pdf"
+)
 # --- CONTACT ---
 st.header("Contact Me")
 st.write("""
